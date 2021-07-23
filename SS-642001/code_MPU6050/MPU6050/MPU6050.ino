31712800dc4b67182735832ce7b30de011db31e8
ponnapajib <87405477+ponnapajib@users.noreply.github.com>
2021-07-23 10:27:04 +0700

    Add files via upload

---
+++ SS-642001/code_MPU6050/MPU6050/MPU6050.ino
@@ -0,0 +1,36 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int valx , valy , valz;
+char rd;
+int prevVal;
+int led = 13 ;
+int pin11 = 4, pin10 = 5;
+int val1 , val2 ;
+int valgy1 = 0 , valgy2 = 0;
+void setup() 
+{
+Wire.begin();
+Serial.begin(38400);
+Serial.println("Initialize MPU");
+mpu.initialize();
+Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
+}
+void loop() 
+{
+mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+valx = map(ax, -17000, 17000, 0, 179);
+valy = map(ay, -17000, 17000, 0, 179);
+valz = map(az, -17000, 17000, 0, 179);
+Serial.print("axis x = ") ; 
+Serial.print(valx) ; 
+Serial.print(" axis y = ") ; 
+Serial.print(valy) ; 
+Serial.print(" axis z = ") ; 
+Serial.println(valz) ; 
+delay(100);
+
+}
